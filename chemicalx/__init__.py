23d51c1472eb6e7bbde4d27cc033906a23ea06d5
Rozemberczki <kmdb028@astrazeneca.net>
2021-12-15 14:07:30 +0000

    Devops

--- chemicalx/__init__.py
+++ chemicalx/__init__.py
@@ -1,2 +1,3 @@
-from chemicalx.models import audnnsynergy, caster, deepcci, deepddi, deepdds, deepdrug, deepsynergy, dpddi, epgcnds
-from chemicalx.models import gcnbmp, matchmaker, mhcaddi, mrhgnn, ssiddi
+from chemicalx.models import (audnnsynergy, caster, deepcci, deepddi, deepdds,
+                              deepdrug, deepsynergy, dpddi, epgcnds, gcnbmp,
+                              matchmaker, mhcaddi, mrhgnn, ssiddi)
