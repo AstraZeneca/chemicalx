d8f341059424c212a888053899a5c87c25b5df12
Rozemberczki <kmdb028@astrazeneca.net>
2021-12-20 14:31:19 +0100

    Dataset outline

--- chemicalx/dataset/__init_.py
+++ chemicalx/dataset/__init_.py
@@ -1,6 +1,6 @@
 from .chchminer import *  # noqa:F401,F403
 from .dataset import *  # noqa:F401,F403
-from .dpddi import *  # noqa:F401,F403
+from .dpddione import *  # noqa:F401,F403
 from .drugcomb import *  # noqa:F401,F403
 from .drugcombdb import *  # noqa:F401,F403
 from .oncolypharm import *  # noqa:F401,F403
--- chemicalx/dataset/dpddione.py
+++ chemicalx/dataset/dpddione.py
@@ -1,3 +1,3 @@
-class DPDDIDataset(object):
+class DPDDIOneDataset(object):
     def __init__(self):
         pass
