6d4ac85b98de8a133591efa033dcd80da92129fe
Rozemberczki <kmdb028@astrazeneca.net>
2021-12-20 14:07:47 +0100

    Dataset outline

--- chemicalx/datasets/__init_.py
+++ chemicalx/datasets/__init_.py
@@ -4,5 +4,5 @@ from .drugcomb import *  # noqa:F401,F403
 from .drugcombdb import *  # noqa:F401,F403
 from .oncolypharm import *  # noqa:F401,F403
 from .synergyxdb import *  # noqa:F401,F403
-from .zhangddi import *  # noqa:F401,F403
 from .twosides import *  # noqa:F401,F403
+from .zhangddi import *  # noqa:F401,F403
