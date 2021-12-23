e3b1f4cd8fe2a643bda826e94a09ffe0b03e610c
Rozemberczki <kmdb028@astrazeneca.net>
2021-12-23 10:31:30 +0100

    Doc update

--- tests/unit/test_datastructures.py
+++ tests/unit/test_datastructures.py
@@ -1,20 +1,9 @@
 import unittest
 
-from chemicalx.models import (
-    CASTER,
-    DPDDI,
-    EPGCNDS,
-    GCNBMP,
-    MHCADDI,
-    MRGNN,
-    SSIDDI,
-    AUDNNSynergy,
-    DeepCCI,
-    DeepDDI,
-    DeepDDS,
-    DeepDrug,
-    DeepSynergy,
-    MatchMaker,
+from chemicalx.data import (
+    ContextFeatureSet,
+    DrugFeatureSet,
+    LabelSet,
 )
 
 
@@ -22,54 +11,14 @@ class TestDataStructures(unittest.TestCase):
     def setUp(self):
         self.x = 2
 
-    def test_CASTER(self):
-        model = CASTER(x=2)
-        assert model.x == 2
+    def test_ContextFeatureSet(self):
+        data = ContextFeatureSet(x=2)
+        assert data.x == 2
 
-    def test_DPDDI(self):
-        model = DPDDI(x=2)
-        assert model.x == 2
+    def test_DrugFeatureSet(self):
+        data = DrugFeatureSet(x=2)
+        assert data.x == 2
 
-    def test_EPGCNDS(self):
-        model = EPGCNDS(x=2)
-        assert model.x == 2
-
-    def test_GCNBMP(self):
-        model = GCNBMP(x=2)
-        assert model.x == 2
-
-    def test_MHCADDI(self):
-        model = MHCADDI(x=2)
-        assert model.x == 2
-
-    def test_MRGNN(self):
-        model = MRGNN(x=2)
-        assert model.x == 2
-
-    def test_SSIDDI(self):
-        model = SSIDDI(x=2)
-        assert model.x == 2
-
-    def test_AUDNNSynergy(self):
-        model = AUDNNSynergy(x=2)
-        assert model.x == 2
-
-    def test_DeepCCI(self):
-        model = DeepCCI(x=2)
-        assert model.x == 2
-
-    def test_DeepDDI(self):
-        model = DeepDDI(x=2)
-        assert model.x == 2
-
-    def test_DeepDrug(self):
-        model = DeepDrug(x=2)
-        assert model.x == 2
-
-    def test_DeepSynergy(self):
-        model = DeepSynergy(x=2)
-        assert model.x == 2
-
-    def test_MatchMaker(self):
-        model = MatchMaker(x=2)
-        assert model.x == 2
+    def test_LabelSet(self):
+        data = LabelSet(x=2)
+        assert data.x == 2
