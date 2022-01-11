56ee8ae383a9d4d7ea5b0c876fb6d2656b20a154
Rozemberczki <kmdb028@astrazeneca.net>
2022-01-11 12:44:23 +0000

    DrugSet

--- chemicalx/data/drugfeatureset.py
+++ chemicalx/data/drugfeatureset.py
@@ -2,160 +2,178 @@ import numpy as np
 from typing import List, Dict
 
 
-class ContextFeatureSet(dict):
+class DrugFeatureSet(dict):
     """
-    Context feature set for biological/chemical context feature vectors.
+    Drug feature set for compunds.
     """
 
-    def __setitem__(self, context: str, features: np.ndarray):
-        """Setting the feature vector for a biological context key.
+    def __setitem__(self, drug: str, features: Dict[str, Union[str, np.ndarray]]):
+        """Setting the features for a compound key
 
         Args:
-            context (str): Biological or chemical context identifier.
-            features (np.ndarray): Feature vector for the context.
+            drug (str): Drug identifier.
+            features (dict): Dictionary of smiles string and molecular features.
         """
-        self.__dict__[context] = features.reshape(1, -1)
+        self.__dict__[drug] = {}
+        self.__dict__[drug]["smiles"] = features["smiles"]
+        self.__dict__[drug]["features"] = features["features"].reshape(-1, 1)
 
-    def __getitem__(self, context: str):
-        """Getting the feature vector for a biological context key.
+    def __getitem__(self, drug: str):
+        """Getting the features for a drug key.
 
         Args:
-            context (str): Biological or chemical context identifier.
+            drug (str): Drug identifier.
         Returns:
-            np.ndarray: The feature vector corresponding to the key.
+            dict: The drug features corresponding to the key.
         """
-        return self.__dict__[context]
+        return self.__dict__[drug]
 
     def __len__(self):
-        """Getting the number of biological/chemical contexts.
+        """Getting the number of drugs.
 
         Returns:
-            int: The number of contexts.
+            int: The number of drugs.
         """
         return len(self.__dict__)
 
-    def __delitem__(self, context: str):
-        """Deleting the feature vector for a biological context key.
+    def __delitem__(self, drug: str):
+        """Deleting the features for a drug key.
 
         Args:
-            context (str): Biological or chemical context identifier.
+            drug (str): Drug identifier.
         """
-        del self.__dict__[context]
+        del self.__dict__[drug]
 
     def clear(self):
-        """Deleting all of the contexts from the context feature set.
+        """Deleting all of the drugs from the drug feature set.
 
         Returns:
-            ContextFeatureSet: An empty context feature set.
+            DrugFeatureSet: An empty drug feature set.
         """
         return self.__dict__.clear()
 
-    def has_context(self, context: str):
-        """Checking whether a context feature set contains a context.
+    def has_drug(self, drug: str):
+        """Checking whether a drug feature set contains a drug.
 
         Args:
-            context (str): Biological or chemical context identifier.
+            drug (str): Drug identifier.
         Returns:
-            bool: Boolean describing whether the context is in the context set.
+            bool: Boolean describing whether the drug is in the drug set.
         """
-        return context in self.__dict__
+        return drug in self.__dict__
 
-    def update(self, data: Dict[str, np.ndarray]):
-        """Updating a dictionary of context keys - feature vector values to a context set.
+    def update(self, data: Dict[str, Dict[str, Union[str, np.ndarray]]]):
+        """Adding a dictionary of drug keys - feature dictionaries to a drug set.
 
         Args:
-            data (dict): A dictionary of context keys with feature vector values.
+            data (dict): A dictionary of drug keys with feature dictionaries.
         Returns:
-            ContextFeatureSet: The updated context feature set.
+            DrugFeatureSet: The updated drug feature set.
         """
-        return self.__dict__.update({context: features.reshape(1, -1) for context, features in data.items()})
+        return self.__dict__.update(
+            {
+                drug: {"smiles": features["smiles"], "features": features["features"].reshape(1, -1)}
+                for drug, features in data.items()
+            }
+        )
 
     def keys(self):
-        """Retrieving the list of biological / chemical contexts in a feature set.
+        """Retrieving the drugs in a feature set.
 
         Returns:
-            list: An iterator of context identifiers.
+            list: An iterator of drug identifiers.
         """
         return self.__dict__.keys()
 
     def values(self):
-        """Retrieving the iterator of context feature vectors.
+        """Retrieving the iterator of drug features.
 
         Returns:
-            list: Feature vector iterator.
+            list: Feature iterator.
         """
         return self.__dict__.values()
 
     def items(self):
-        """Retrieving the iterator of tuples containing context identifier - feature vector pairs.
+        """Retrieving the iterator of tuples containing drug identifier - feature pairs.
 
         Returns:
-            list: An iterator of (context - feature vector) tuples.
+            list: An iterator of (drug - feature dictionary) tuples.
         """
         return self.__dict__.items()
 
-    def __contains__(self, context: str):
+    def __contains__(self, drug: str):
         """A data class method which allows the use of the 'in' operator.
 
         Args:
-            context (str): A context identifier.
+            drug (str): A drug identifier.
         Returns:
-            bool: An indicator whether the context is in the context feature set.
+            bool: An indicator whether the drug is in the drug feature set.
         """
-        return context in self.__dict__
+        return drug in self.__dict__
 
     def __iter__(self):
-        """A data class method which allows iteration over the context feature set.
+        """A data class method which allows iteration over the drug feature set.
 
         Returns:
-            iterable: An iterable of the context feature set.
+            iterable: An iterable of the drug feature set.
         """
         return iter(self.__dict__)
 
-    def get_context_count(self) -> int:
-        """Getting the number of biological contexts.
+    def get_drug_count(self) -> int:
+        """Getting the number of drugs.
 
         Returns:
-            int: The number of contexts.
+            int: The number of drugs.
         """
         return len(self.__dict__)
 
-    def get_context_feature_count(self) -> int:
-        """Getting the number of context feature dimensions.
+    def get_drug_feature_count(self) -> int:
+        """Getting the number of drug feature dimensions.
 
         Returns:
-            feature_count (int): The number of context feature dimensions.
+            feature_count (int): The number of drug feature dimensions.
         """
         feature_count = None
         if len(self.__dict__) > 0:
-            contexts = list(self.keys())
-            first_context = contexts[0]
-            feature_vector = self.__dict__[first_context]
+            drugs = list(self.keys())
+            first_drug = drugs[0]
+            feature_vector = self.__dict__[drugs[0]]["features"]
             feature_count = feature_vector.shape[1]
         return feature_count
 
-    def get_feature_matrix(self, contexts: List[str]) -> np.ndarray:
-        """Getting the feature matrix for a list of contexts.
+    def get_feature_matrix(self, drugs: List[str]) -> np.ndarray:
+        """Getting the drug feature matrix for a list of drugs.
 
         Args:
-            contexts (list): A list of context identifiers.
+            drugs (list): A list of drug identifiers.
         Return:
-            features (np.ndarray): A matrix of context features.
+            features (np.ndarray): A matrix of drug features.
         """
-        features = np.concatenate([self.__dict__[context] for context in contexts])
+        features = np.concatenate([self.__dict__[drug]["features"] for drug in drugs])
+        return features
+
+    def get_smiles_strings(self, drugs: List[str]) -> List[str]:
+        """Getting the list of drugs.
+
+        Args:
+            drugs (list): A list of drug identifiers.
+        Return:
+            smiles_strings (list): A list of smiles strings for the drugs.
+        """
+        features = np.concatenate([self.__dict__[drug]["smiles"] for drug in drugs])
         return features
 
     def get_feature_density_rate(self) -> float:
-        """Getting the ratio of non-zero feature values in the context feature matrix.
+        """Getting the ratio of non-zero drug feature values in the drug feature matrix.
 
         Returns:
-            float: The ratio of non-zero entries in the whole context feature matrix.
+            float: The ratio of non-zero entries in the whole drug feature matrix.
         """
         feature_matrix_density = None
         if len(self.__dict__) > 0:
-            all_contexts = list(self.keys())
-            feature_matrix = self.get_feature_matrix(all_contexts)
+            all_drugs = list(self.keys())
+            feature_matrix = self.get_feature_matrix(all_drugs)
             non_zero_count = np.sum(feature_matrix != 0)
-            context_count, feature_count = feature_matrix.shape
-            feature_matrix_density = non_zero_count / (feature_count * context_count)
+            drug_count, feature_count = feature_matrix.shape
+            feature_matrix_density = non_zero_count / (feature_count * drug_count)
         return feature_matrix_density
