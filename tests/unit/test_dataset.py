fbff5d9899e140cda8409bd284e02db90e2f28f6
Rozemberczki <kmdb028@astrazeneca.net>
2022-01-11 12:58:35 +0000

    DrugSet

--- tests/unit/test_dataset.py
+++ tests/unit/test_dataset.py
@@ -6,13 +6,27 @@ class TestDrugComb(unittest.TestCase):
     def setUp(self):
         self.dataset_loader = DatasetLoader("drugcomb")
 
-    def test_BaseDataset(self):
-        assert 2 == 2
+    def test_get_context_features(self):
+        context_feature_set = self.dataset_loader.get_context_features()
+
+        assert len(context_feature_set) == 288
+
+    def test_get_drug_features(self):
+        drug_feature_set = self.dataset_loader.get_drug_features()
+
+        assert len(drug_feature_set) == 4146
 
 
 class TestDrugCombDB(unittest.TestCase):
     def setUp(self):
         self.dataset_loader = DatasetLoader("drugcombdb")
 
-    def test_BaseDataset(self):
-        assert 2 == 2
+    def test_get_context_features(self):
+        context_feature_set = self.dataset_loader.get_context_features()
+
+        assert len(context_feature_set) == 112
+
+    def test_get_drug_features(self):
+        drug_feature_set = self.dataset_loader.get_drug_features()
+
+        assert len(drug_feature_set) == 2956
