b80084fb20e6862e56cc570d6f2b4df6d2adcf2c
Rozemberczki <kmdb028@astrazeneca.net>
2022-01-12 14:50:34 +0000

    ChemicalX

--- chemicalx/data/drugpairbatch.py
+++ chemicalx/data/drugpairbatch.py
@@ -1,4 +1,17 @@
 class DrugPairBatch:
+    """
+    A data class to store a labeled drug pair batch.
+
+    Args:
+        identifiers (pd.DataFrame or None): A dataframe with drug pair, context and label columns.
+        drug_features_left (torch.FloatTensor or None): A matrix of molecular features for the left hand drugs.
+        drug_molecules_left (torchdrug.PackedGraph or None): Packed molecules for the left hand drugs.
+        drug_features_right (torch.FloatTensor or None): A matrix of molecular features for the right hand drugs.
+        drug_molecules_right (torchdrug.PackedGraph or None): Packed molecules for the right hand drugs.
+        context_features (torch.FloatTensor or None): A matrix of biological/chemical context features.
+        labels (torch.FloatTensor or None): A vector of drug pair labels.
+    """
+
     def __init__(
         self,
         identifiers=None,
