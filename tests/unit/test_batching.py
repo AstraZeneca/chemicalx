ae3e593b312a945f5815ae51d869c88b4c5ffc41
Rozemberczki <kmdb028@astrazeneca.net>
2022-01-12 14:53:50 +0000

    ChemicalX

--- tests/unit/test_batching.py
+++ tests/unit/test_batching.py
@@ -23,3 +23,18 @@ class TestGeneratorDrugCombDB(unittest.TestCase):
         for batch in generator:
             assert batch.drug_features_left.shape[1] == 256
             assert (batch.drug_features_left.shape[0] == 2975) or (batch.drug_features_left.shape[0] == 4096)
+
+    def set_all_false(self):
+        generator = BatchGenerator(
+            batch_size=4096, context_features=True, drug_features=True, drug_molecules=True, labels=True
+        )
+
+        generator.set_data(
+            context_feature_set=self.context_feature_set,
+            drug_feature_set=self.drug_feature_set,
+            labeled_triples=self.labeled_triples,
+        )
+
+        for batch in generator:
+            assert batch.drug_features_left.shape[1] == 256
+            assert (batch.drug_features_left.shape[0] == 2975) or (batch.drug_features_left.shape[0] == 4096)
