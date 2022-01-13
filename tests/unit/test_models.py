4a1d70124ee36b3786da19567c8d60379b33f95d
Rozemberczki <kmdb028@astrazeneca.net>
2022-01-13 18:32:03 +0000

    EPGCN-Tests

--- tests/unit/test_models.py
+++ tests/unit/test_models.py
@@ -26,7 +26,7 @@ class TestModels(unittest.TestCase):
         drug_feature_set = loader.get_drug_features()
         context_feature_set = loader.get_context_features()
         labeled_triples = loader.get_labeled_triples()
-        labeled_triples, _ = labeled_triples.train_test_split(train_size=0.02)
+        labeled_triples, _ = labeled_triples.train_test_split(train_size=0.005)
         self.generator = BatchGenerator(
             batch_size=32, context_features=True, drug_features=True, drug_molecules=True, labels=True
         )
