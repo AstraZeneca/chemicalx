c65913b7f8c2e09ba4f8814ced96eea619b94eac
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-01-17 01:00:20 +0100

    Update __init__.py

--- chemicalx/data/__init__.py
+++ chemicalx/data/__init__.py
@@ -2,17 +2,11 @@
 
 from class_resolver import Resolver
 
-from .batchgenerator import *  # noqa:F401,F403
 from .batchgenerator import BatchGenerator
-from .contextfeatureset import *  # noqa:F401,F403
 from .contextfeatureset import ContextFeatureSet
-from .datasetloader import DrugCombDB  # noqa:F401,F403
-from .datasetloader import DatasetLoader, DrugComb
-from .drugfeatureset import *  # noqa:F401,F403
+from .datasetloader import DatasetLoader, DrugComb, DrugCombDB
 from .drugfeatureset import DrugFeatureSet
-from .drugpairbatch import *  # noqa:F401,F403
 from .drugpairbatch import DrugPairBatch
-from .labeledtriples import *  # noqa:F401,F403
 from .labeledtriples import LabeledTriples
 
 __all__ = [
