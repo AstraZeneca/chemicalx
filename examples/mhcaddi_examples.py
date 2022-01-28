059369e97c9af19ccfc44f801c707dc27f2b2014
Sebastian Nilsson <sebastian.nilsson@astrazeneca.com>
2022-01-28 17:16:54 +0000

    Added example file for running mhcaddi

---
+++ examples/mhcaddi_examples.py
@@ -0,0 +1,27 @@
+"""Example with DeepSynergy."""
+
+from chemicalx import pipeline
+from chemicalx.data.datasetloader import TwoSides
+from chemicalx.models.mhcaddi import MHCADDI
+from torchdrug.data.molecule import Molecule
+
+
+def main():
+    """Train and evaluate the DeepSynergy model."""
+    dataset = TwoSides()
+    model = MHCADDI(d_atom_feat=69, n_atom_type=100, n_bond_type=12)
+
+    results = pipeline(
+        dataset=dataset,
+        model=model,
+        batch_size=2,
+        epochs=1,
+        context_features=False,
+        drug_features=True,
+        drug_molecules=True,
+    )
+    results.summarize()
+
+
+if __name__ == "__main__":
+    main()
