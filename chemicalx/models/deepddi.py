20f0a50240402b9111ae98dc6d4c3863ca00152c
hzcheney <chenhan199781@gmail.com>
2022-01-31 01:13:00 +0800

    Style: deepddi model

--- chemicalx/models/deepddi.py
+++ chemicalx/models/deepddi.py
@@ -37,28 +37,19 @@ class DeepDDI(Model):
         dnn = [
             torch.nn.Linear(drug_channels * 2, hidden_channels),
             torch.nn.ReLU(),
-            torch.nn.BatchNorm1d(
-                num_features=hidden_channels,
-                affine=True,
-                momentum=None
-            ),
-            torch.nn.ReLU()
+            torch.nn.BatchNorm1d(num_features=hidden_channels, affine=True, momentum=None),
+            torch.nn.ReLU(),
         ]
         for _ in range(hidden_layers_num - 1):
-            dnn.extend([
-                torch.nn.Linear(hidden_channels, hidden_channels),
-                torch.nn.ReLU(),
-                torch.nn.BatchNorm1d(
-                    num_features=hidden_channels,
-                    affine=True,
-                    momentum=None
-                ),
-                torch.nn.ReLU()
-            ])
-        dnn.extend([
-            torch.nn.Linear(hidden_channels, out_channels),
-            torch.nn.Sigmoid()
-        ])
+            dnn.extend(
+                [
+                    torch.nn.Linear(hidden_channels, hidden_channels),
+                    torch.nn.ReLU(),
+                    torch.nn.BatchNorm1d(num_features=hidden_channels, affine=True, momentum=None),
+                    torch.nn.ReLU(),
+                ]
+            )
+        dnn.extend([torch.nn.Linear(hidden_channels, out_channels), torch.nn.Sigmoid()])
         self.dnn = torch.nn.Sequential(*dnn)
 
     def unpack(self, batch: DrugPairBatch):
@@ -69,9 +60,9 @@ class DeepDDI(Model):
         )
 
     def forward(
-            self,
-            drug_features_left: torch.FloatTensor,
-            drug_features_right: torch.FloatTensor,
+        self,
+        drug_features_left: torch.FloatTensor,
+        drug_features_right: torch.FloatTensor,
     ) -> torch.FloatTensor:
         """
         Run a forward pass of the DeepDDI model.
