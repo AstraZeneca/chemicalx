235107b577d10d509fe9a51df96b0fc752ac9542
Piotr Grabowski <piotr.grabowski@astrazeneca.com>
2022-02-02 10:03:10 +0000

    removed context feat support in DeepDrug

--- chemicalx/models/deepdrug.py
+++ chemicalx/models/deepdrug.py
@@ -25,7 +25,6 @@ class DeepDrug(Model):
     def __init__(
         self,
         *,
-        context_channels: int = 112,
         molecule_channels: int = TORCHDRUG_NODE_FEATURES,
         num_gcn_layers: int = 4,
         gcn_layer_hidden_size: int = 64,
@@ -34,8 +33,6 @@ class DeepDrug(Model):
     ):
         """Instantiate the DeepDrug model.
 
-        :param context_channels: The number of contexts used for learning. Set to 0 if not using any context. In that
-        case make sure you're not calling forward() with the context feature tensor.
         :param molecule_channels: The number of molecular features.
         :param num_gcn_layers: Number of GCN layers.
         :param gcn_layer_hidden_size: number of hidden units in GCN layers
@@ -55,30 +52,27 @@ class DeepDrug(Model):
             )
 
         self.max_readout = MaxReadout()
-        self.middle_channels = 2 * self.gcn_layer_hidden_size + context_channels  # left/right feats + context
+        self.middle_channels = 2 * self.gcn_layer_hidden_size
         self.dropout = torch.nn.Dropout(p=dropout_rate)
         self.batch_norm = torch.nn.BatchNorm1d(self.middle_channels)
         self.final = torch.nn.Linear(self.middle_channels, out_channels)
-        self.context_channels = context_channels
 
     def unpack(self, batch: DrugPairBatch):
-        """Return the context features, left drug molecules, and right drug molecules."""
-        if self.context_channels > 0:
-            return (
-                batch.context_features,
-                batch.drug_molecules_left,
-                batch.drug_molecules_right,
-            )
-        else:
-            return (
-                batch.drug_molecules_left,
-                batch.drug_molecules_right,
-            )
+        """Return the left drug molecules, and right drug molecules."""
+        return (
+            batch.drug_molecules_left,
+            batch.drug_molecules_right,
+        )
 
-    def forward(
-        self, context_features: Optional[torch.FloatTensor], molecules_left: PackedGraph, molecules_right: PackedGraph
-    ) -> torch.FloatTensor:
+    def forward(self, molecules_left: PackedGraph, molecules_right: PackedGraph) -> torch.FloatTensor:
+        """
+        Run a forward pass of the DeepDrug model.
 
+        :param molecules_left: Batched molecules for the left side drugs.
+        :param molecules_right: Batched molecules for the right side drugs.
+
+        :return: A column vector of predicted synergy scores.
+        """
         features_left = self.graph_convolution_first(molecules_left, molecules_left.data_dict["node_feature"])
         features_right = self.graph_convolution_first(molecules_right, molecules_right.data_dict["node_feature"])
 
@@ -90,10 +84,7 @@ class DeepDrug(Model):
         features_left = self.max_readout(molecules_left, features_left)
         features_right = self.max_readout(molecules_right, features_right)
 
-        if self.context_channels > 0:
-            hidden = torch.cat([features_left, features_right, context_features], dim=1)
-        else:
-            hidden = torch.cat([features_left, features_right], dim=1)
+        hidden = torch.cat([features_left, features_right], dim=1)
         hidden = self.batch_norm(hidden)
         hidden = self.dropout(hidden)
         hidden = self.final(hidden)
