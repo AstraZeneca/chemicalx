03a1d205ce56b0c3898ccadc3005b8e4bb4b63b8
Michaël Ughetto <michael.ughetto@astrazeneca.com>
2022-02-02 13:24:16 +0000

    Fixing the citation back to what is in main

--- chemicalx/models/gcnbmp.py
+++ chemicalx/models/gcnbmp.py
@@ -189,10 +189,12 @@ class GCNBMPEncoder(nn.Module, core.Configurable):
 
 
 class GCNBMP(Model):
-    r"""The GCNBMP model from [gcnbmp].
+    """An implementation of the GCN-BMP model from [chen2020]_.
 
-    .. [gcnbmp] `GCN-BMP: Investigating graph representation learning for DDI prediction task
-       <https://www.sciencedirect.com/science/article/pii/S1046202320300608>`_
+    .. seealso:: This model was suggested in https://github.com/AstraZeneca/chemicalx/issues/21
+    
+    .. [chen2020] Chen, X., *et al.* (2020). `GCN-BMP: Investigating graph representation learning
+       for DDI prediction task <https://doi.org/10.1016/j.ymeth.2020.05.014>`_. *Methods*, 179, 47–54.
     """
 
     def __init__(
