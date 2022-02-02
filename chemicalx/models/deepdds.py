0858fefb3358896d95020db7256c80d2f128b23e
Charles Tapley Hoyt <cthoyt@gmail.com>
2022-02-02 13:15:44 +0100

    Begin adding proper citations

--- chemicalx/models/deepdds.py
+++ chemicalx/models/deepdds.py
@@ -8,7 +8,11 @@ __all__ = [
 
 
 class DeepDDS(UnimplementedModel):
-    """An implementation of the DeepDDS model.
+    """An implementation of the DeepDDS model from [wang2021]_.
 
     .. seealso:: https://github.com/AstraZeneca/chemicalx/issues/19
+
+    .. [wang2021] Wang, J., *et al.* (2021). `DeepDDS: deep graph neural network with attention
+       mechanism to predict synergistic drug combinations <http://arxiv.org/abs/2107.02467>`_.
+       *arXiv*, 2107.02467.
     """
--- chemicalx/models/deepdrug.py
+++ chemicalx/models/deepdrug.py
@@ -8,7 +8,11 @@ __all__ = [
 
 
 class DeepDrug(UnimplementedModel):
-    """An implementation of the DeepDrug model.
+    """An implementation of the DeepDrug model from [cao2020]_.
 
     .. seealso:: https://github.com/AstraZeneca/chemicalx/issues/14
+
+    .. [cao2020] Cao, X., *et al.* (2020). `DeepDrug: A general graph-based deep learning framework
+       for drug relation prediction <https://doi.org/10.1101/2020.11.09.375626>`_.
+       *bioRxiv*, 2020.11.09.375626.
     """
