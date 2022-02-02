e461fbf1147adf67bdf34725ec79257461878c85
Rozemberczki <kmdb028@astrazeneca.net>
2022-02-02 15:35:45 +0000

    GCNBMP

--- chemicalx/models/gcnbmp.py
+++ chemicalx/models/gcnbmp.py
@@ -175,7 +175,9 @@ class GCNBMPEncoder(nn.Module, core.Configurable):
 
 class GCNBMP(Model):
     """An implementation of the GCN-BMP model from [chen2020]_.
+
     .. seealso:: This model was suggested in https://github.com/AstraZeneca/chemicalx/issues/21
+
     .. [chen2020] Chen, X., *et al.* (2020). `GCN-BMP: Investigating graph representation learning
        for DDI prediction task <https://doi.org/10.1016/j.ymeth.2020.05.014>`_. *Methods*, 179, 47–54.
     """
